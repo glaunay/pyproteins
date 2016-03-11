6bcd51572ba848c48cc22c4beae22dbdef1f9a5a
Guillaume Launay <guillaumelaunay@ibcp2120.ibcp.fr>
2016-03-11 15:53:12 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
-  name = 'pySeq',
-  packages = ['pySeq'], # this must be the same as the name above
+  name = 'pyproteins',
+  packages = find_packages(), # this must be the same as the name above
   version = '0.1',
   description = 'Toolbox to manipulate protein sequence data',
   author = 'Guillaume Launay',
   author_email = 'pitooon@gmail.com',
-  url = 'https://github.com/glaunay/pySeq', # use the URL to the github repo
-  download_url = 'https://github.com/glaunay/pySeq/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/glaunay/pyproteins', # use the URL to the github repo
+  download_url = 'https://github.com/glaunay/pyproteins/tarball/0.1', # I'll explain this in a second
   keywords = ['protein', 'sequence'], # arbitrary keywords
   classifiers = [],
   install_requires=[
           'bs4', 'Bio', 'urllib2', 'numpy'
       ],
+    dependency_links = [
+        "http://dev.danse.us/trac/pathos"
+    ]
 )
