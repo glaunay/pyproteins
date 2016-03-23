cfabc209ee2cf4b7df7f45b175ae9a477478ec39
glaunay <pitooon@gmail.com>
2016-03-23 13:42:09 +0100

    package restructured

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup, find_packages
 setup(
   name = 'pyproteins',
   packages = find_packages(), # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
+  license='BSD',
   description = 'Toolbox to manipulate protein sequence data',
   author = 'Guillaume Launay',
   author_email = 'pitooon@gmail.com',
   url = 'https://github.com/glaunay/pyproteins', # use the URL to the github repo
-  download_url = 'https://github.com/glaunay/pyproteins/tarball/0.1', # I'll explain this in a second
+  packages=find_packages('src'),
+  package_dir={'': 'src'},
+  include_package_data=True,
+  zip_safe=False,
+  py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+  download_url = 'https://github.com/glaunay/pyproteins/tarball/0.2', # I'll explain this in a second
   keywords = ['protein', 'sequence'], # arbitrary keywords
   classifiers = [],
   install_requires=[
