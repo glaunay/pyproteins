265cebb2eac6b84642cc94685ea4029b28517c37
glaunay <pitooon@gmail.com>
2021-01-26 16:02:57 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -21,8 +21,9 @@ setup(
   author = 'Guillaume Launay & Cecile Hilpert',
   author_email = 'pitooon@gmail.com',
   url = 'https://github.com/glaunay/pyproteins', # use the URL to the github repo
-  packages=find_packages('src'),
   package_dir={'': 'src'},
+  #packages=find_packages('src'),
+  packages=['pyproteins'],
   include_package_data=True,
   zip_safe=False,
   py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
